0c2810f2eb4aadc220dbfc8b1617c09a24ffebd8
pmalviya <poorva0902@gmail.com>
2014-10-18 08:50:08 -0700

    adding logout action

--- tastebuds/src/com/codepath/apps/tastebuds/activities/HomeActivity.java
+++ tastebuds/src/com/codepath/apps/tastebuds/activities/HomeActivity.java
@@ -3,14 +3,19 @@ package com.codepath.apps.tastebuds.activities;
 import java.util.ArrayList;
 
 import com.codepath.apps.tastebuds.R;
+import com.codepath.apps.tastebuds.fragments.DishReviewDialog;
 import com.codepath.apps.tastebuds.fragments.FriendsListFragment;
 import com.codepath.apps.tastebuds.fragments.RestaurantReviewDialog;
+import com.codepath.apps.tastebuds.fragments.DishReviewDialog.DishReviewDialogListener;
 import com.codepath.apps.tastebuds.fragments.RestaurantReviewDialog.RestaurantReviewDialogListener;
 import com.codepath.apps.tastebuds.fragments.RestaurantListFragment;
 import com.codepath.apps.tastebuds.fragments.RestaurantMapListFragment;
 import com.codepath.apps.tastebuds.listeners.FragmentTabListener;
+import com.codepath.apps.tastebuds.models.Dish;
+import com.codepath.apps.tastebuds.models.DishReview;
 import com.codepath.apps.tastebuds.models.Restaurant;
 import com.codepath.apps.tastebuds.models.RestaurantReview;
+import com.facebook.Session;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
@@ -19,14 +24,17 @@ import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 public class HomeActivity extends FragmentActivity {
-	
+
 	private ParseUser user;
 	ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
 
@@ -37,45 +45,69 @@ public class HomeActivity extends FragmentActivity {
 		Log.d("Debug", "Home Activity OnCreate");
 		user = ParseUser.getCurrentUser();
 		setupTabs();
-		
+
 	}
-	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+	public void onLogoutAction(MenuItem mi){
+		Session session = Session.getActiveSession();
+		if (session != null) {
+
+			if (!session.isClosed()) {
+				session.closeAndClearTokenInformation();
+				//clear your preferences if saved
+				startActivity(new Intent(getApplicationContext(), TastebudsLoginActivity.class));
+				// make sure the user can not access the page after he/she is logged out
+				// clear the activity stack
+				finish();
+			}
+		} else {
+
+			session.closeAndClearTokenInformation();
+			//clear your preferences if saved
+
+		}
+	}
 	private void setupTabs() {
 		ActionBar actionBar = getActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		actionBar.setDisplayShowTitleEnabled(true);
 
 		Tab tab1 = actionBar
-			.newTab()
-			.setText("Restaurants")
-			.setIcon(R.drawable.ic_launcher)
-			.setTag("RestaurantListFragment")
-			.setTabListener(
-				new FragmentTabListener<RestaurantListFragment>(R.id.ctRestaurantsLists, this, "first",
+				.newTab()
+				.setText("Restaurants")
+				.setIcon(R.drawable.ic_launcher)
+				.setTag("RestaurantListFragment")
+				.setTabListener(
+						new FragmentTabListener<RestaurantListFragment>(R.id.ctRestaurantsLists, this, "first",
 								RestaurantListFragment.class));
 
 		actionBar.addTab(tab1);
 		actionBar.selectTab(tab1);
 
 		Tab tab2 = actionBar
-			.newTab()
-			.setText("Friends")
-			.setIcon(R.drawable.ic_launcher)
-			.setTag("FriendsListFragment")
-			.setTabListener(
-			    new FragmentTabListener<FriendsListFragment>(R.id.ctRestaurantsLists, this, "second",
+				.newTab()
+				.setText("Friends")
+				.setIcon(R.drawable.ic_launcher)
+				.setTag("FriendsListFragment")
+				.setTabListener(
+						new FragmentTabListener<FriendsListFragment>(R.id.ctRestaurantsLists, this, "second",
 								FriendsListFragment.class));
 
 		actionBar.addTab(tab2);
 	}
 
 	public void onReview(View view) {
-	    FragmentTransaction ft = getFragmentManager().beginTransaction();
-	    Fragment prev = getFragmentManager().findFragmentByTag("dialog");
-	    if (prev != null) {
-	        ft.remove(prev);
-	    }
-	    ft.addToBackStack(null);
+		FragmentTransaction ft = getFragmentManager().beginTransaction();
+		Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+		if (prev != null) {
+			ft.remove(prev);
+		}
+		ft.addToBackStack(null);
 		RestaurantReviewDialog dialog = RestaurantReviewDialog.newInstance(
 				"Shree Datta", "czswrtv");
 		dialog.show(ft, "compose");
@@ -92,8 +124,8 @@ public class HomeActivity extends FragmentActivity {
 							Toast.makeText(HomeActivity.this, "Saved Review FAiled" + arg0.toString(), Toast.LENGTH_LONG).show();
 						}
 					}
-					
-					
+
+
 				});
 			}
 		};
--- tastebuds/src/com/codepath/apps/tastebuds/activities/TastebudsLoginActivity.java
+++ tastebuds/src/com/codepath/apps/tastebuds/activities/TastebudsLoginActivity.java
@@ -81,12 +81,7 @@ public class TastebudsLoginActivity extends Activity {
 		}
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
-		return true;
-	}
+
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
