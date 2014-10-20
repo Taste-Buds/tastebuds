4545df9c2bb175d8b72816ad7dacfa518a7b4002
Shawn Yapa <shawnyapa@gmail.com>
2014-10-19 18:56:11 -0700

    Updated Restaurant Location - BUG in Lat/Long

--- tastebuds/src/com/codepath/apps/tastebuds/models/Restaurant.java
+++ tastebuds/src/com/codepath/apps/tastebuds/models/Restaurant.java
@@ -7,6 +7,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.location.Location;
+import android.util.Log;
+
 public class Restaurant implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -14,8 +17,10 @@ public class Restaurant implements Serializable {
 	private String name; 			// name
 	private String address;			// formatted_address
 	private String phone;			// formatted_phone_number
+	private Location location;		// Restaurant Location
 	private double latitude;		// geometry:location:lat
 	private double longitude;		// geometry:location:lng
+	private float currentDistancetoUser; // Calculated Distance from Current User Location to Restaurant Location
 	private double google_rating;	// rating
 	private int price_level;		// price_level
 	private String website;			// website
@@ -85,8 +90,20 @@ public class Restaurant implements Serializable {
 
 	public String getName() {
 		return name;
-	}	
+	}
 	
+	public Location getLocation() {
+		return location;
+	}
+	
+	public float getCurrentDistancetoUser() {
+		return currentDistancetoUser;
+	}
+
+	public void setCurrentDistancetoUser(float currentDistancetoUser) {
+		this.currentDistancetoUser = currentDistancetoUser;
+	}
+
 	public static Restaurant fromJSON(JSONObject jsonObject) {
 		Restaurant restaurant = new Restaurant();
 		try {
@@ -106,13 +123,49 @@ public class Restaurant implements Serializable {
 			restaurant.setIcon(jsonObject.getString("icon"));
 			restaurant.setGoogleReviews(jsonObject.getJSONArray("reviews"));
 			restaurant.setPhotos(jsonObject.getJSONArray("photos"));
-			restaurant.place_id = jsonObject.getString("place_id"); // place_id
-			restaurant.name = jsonObject.getString("name");			// name
-			restaurant.address = jsonObject.getString("formatted_address");		// formatted_address
-			restaurant.phone = jsonObject.getString("formatted_phone_number");	// formatted_phone_number
+			
+			restaurant.place_id = jsonObject.getString("place_id");
+			
+			try {restaurant.name = jsonObject.getString("name");} 
+				catch (Exception e) { restaurant.name = "N/A"; }  // name
+			
+			try {restaurant.address = jsonObject.getString("formatted_address");}
+				catch (Exception e) { restaurant.address = "N/A"; }// formatted_address
+			
+			try {restaurant.phone = jsonObject.getString("formatted_phone_number");}
+				catch (Exception e) { restaurant.phone = "N/A"; }// formatted_phone_number
+			
+			try {
+				JSONObject location = jsonObject.getJSONObject("geometry").getJSONObject("location");
+				restaurant.latitude = location.getDouble("lat");
+				//restaurant.latitude = Double.parseDouble(jsonObject.getJSONObject("geometry").getJSONObject("location").getString("lat"));
+				//restaurant.latitude = jsonObject.getJSONObject("geometry").getJSONObject("location").getDouble("lat");
+				Log.d("Debug", "Lat: " + Double.toString(restaurant.getLatitude()));
+				}
+				catch (Exception e) { 
+					Log.d("Debug", "Can't get Location A");
+					restaurant.latitude = 0; }
+			
+			try {
+				//restaurant.longitude = Double.parseDouble(jsonObject.getJSONObject("geometry").getJSONObject("location").getString("lng"));
+				restaurant.longitude = jsonObject.getJSONObject("geometry").getJSONObject("location").getDouble("lng");
+				}
+			catch (Exception e) { restaurant.longitude = 0; }		
+			
+			if ((restaurant.getLatitude() != 0) && (restaurant.getLongitude() != 0)) {
+				Location location = new Location("");
+			    location.setLatitude(restaurant.getLatitude());
+			    location.setLongitude(restaurant.getLongitude()); 
+			    restaurant.location = location;
+			} else {
+				Log.d("Debug", "No Location");
+			}
+				
 			//restaurant.latitude = Double.parseDouble(jsonObject.getJSONObject("geometry").getJSONObject("location").getString("lat"));		// geometry:location:lat
-			//restaurant.longitude = Doub le.parseDouble(jsonObject.getJSONObject("geometry").getJSONObject("location").getString("lng"));	// geometry:location:lng
-			if ( jsonObject.has("rating")&& jsonObject.getString("rating") != null) {
+			//restaurant.longitude = Double.parseDouble(jsonObject.getJSONObject("geometry").getJSONObject("location").getString("lng"));	// geometry:location:lng
+			
+			//restaurant.location = jsonObject.getJSONObject("geometry").getJSONObject("location");
+			if (jsonObject.getString("rating") != null) {
 				restaurant.google_rating = Double.parseDouble(jsonObject.getString("rating"));	// rating
 			}
 			if (jsonObject.has("price_level") && jsonObject.getString("price_level") != null) {
@@ -128,7 +181,9 @@ public class Restaurant implements Serializable {
 					jsonObject.getJSONObject("opening_hours").getString("open_now") != null) {
 				restaurant.open_now = Boolean.parseBoolean(jsonObject.getJSONObject("opening_hours").getString("open_now"));		// opening_hours:open_now
 			}
-		} catch (JSONException e) {
+		
+		}
+			catch (JSONException e) {
 			e.printStackTrace();
 		}
 		return restaurant;
