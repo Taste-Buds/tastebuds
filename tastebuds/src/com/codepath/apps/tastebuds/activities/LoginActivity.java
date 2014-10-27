c92423af382b0dbcf34a039ee46803b13a09809c
Sushant <sushant.narsale2603@gmail.com>
2014-10-26 23:02:40 -0700

    Styled login page

--- tastebuds/src/com/codepath/apps/tastebuds/activities/LoginActivity.java
+++ tastebuds/src/com/codepath/apps/tastebuds/activities/LoginActivity.java
@@ -50,4 +50,13 @@ public class LoginActivity extends OAuthLoginActivity<GoogleClient> {
 		overridePendingTransition(R.anim.right_in, R.anim.left_out);
 	}
 	
+    @Override
+    public void onBackPressed() {    	
+    	Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//***Change Here***
+        startActivity(intent);
+        finish();
+        System.exit(0);
+    }
 }
